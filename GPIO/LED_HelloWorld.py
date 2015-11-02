3593fd2b663eed2ba9e4f27e2a66fe08f63e665b
Vlad Burca <vladburca2005@gmail.com>
2015-11-02 01:15:33 -0500

    First LED script

---
+++ GPIO/LED_HelloWorld.py
@@ -0,0 +1,33 @@
+import sys, argparse
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+def turnOnLED():
+  GPIO.output(17, GPIO.HIGH)
+
+def turnOffLED():
+  GPIO.output(17, GPIO.LOW)
+
+
+def main():
+  print "Starting up program for controlling GPIO LED on RPi..."
+  
+  parser = argparse.ArgumentParser(description="Control GPIO LED on Raspberry Pi")
+
+  group= parser.add_mutually_exclusive_group(required=True)
+  group.add_argument('--on', action='store_true', help='turn ON the LED')
+  group.add_argument('--off', action='store_false', help='turn OFF the LED')  
+
+  args = parser.parse_args()
+  
+  if args.on:
+    print "Turning ON LED..."
+    turnOnLED()    
+  else:
+    print "Turning OFF LED..."
+    turnOffLED()
+  print "Closing..."
+
+main()
